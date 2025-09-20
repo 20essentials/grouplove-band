fd673f5d07bf2384b18e067792e0e6794750f46b
DominicDev <s0l0estudi4r@gmail.com>
2025-09-20 23:21:58 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'GROUPLOVE';
+export let nameSongs = [
+	"TONGUE TIED", /*1*/
+	"ITCHIN' ON A PHOTOGRAPH", /*2*/
+	"COLOURS", /*3*/
+	"LOVELY CUP", /*4*/
+	"LOVE WILL SAVE YOUR SOUL", /*5*/
+	"STANDING IN THE SUN", /*6*/
+	"TORSO", /*7*/
+	"SIT STILL", /*8*/
+	"WHAT I KNOW", /*9*/
+	"GOLD COAST", /*10*/
+	"SPUN", /*11*/
+	"I'M WITH YOU", /*12*/
+	"WAYS TO GO", /*13*/
+	"CLOSE YOUR EYES", /*14*/
+	"NAKED KIDS", /*15*/
+	"CHLOE", /*16*/
+	"CRUEL AND BEAUTIFUL WORLD", /*17*/
+	"BORDERLINES AND ALIENS", /*18*/
+	"WELCOME TO YOUR LIFE", /*19*/
+	"SPINNING", /*20*/
+]
